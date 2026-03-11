1924e2e9461e2b0bda9996f6f9ab2c4dc1ebcc4f
timur <muhamedzanovtimur1@gmail.com>
2026-03-11 15:32:04 +0600

    cookie service for auth

--- backend/src/common/auth/auth.controller.ts
+++ backend/src/common/auth/auth.controller.ts
@@ -11,6 +11,7 @@ import { AuthService } from './auth.service'
 import { SignupDto } from './dto/signup.dto'
 import { LoginDto } from './dto/login.dto'
 import { JwtAuthGuard } from './guards/jwt-auth.guard'
+import { CookieService } from '../cookies/cookie.service'
 
 @Controller('auth/admin')
 export class AuthController {
@@ -22,11 +23,7 @@ export class AuthController {
 
     //TODO: CookieService
     // git fetch && git checkout main && git pull && git checkout feature/admin-auth && git merge
-    res.setCookie('access_token', token, {
-      httpOnly: true,
-      secure: false,
-      sameSite: 'lax',
-    })
+    CookieService.assignAuthToken(res, token)
 
     return admin
   }
@@ -35,11 +32,7 @@ export class AuthController {
   async login(@Body() dto: LoginDto, @Res({ passthrough: true }) res: any) {
     const { admin, token } = await this.authService.login(dto)
 
-    res.setCookie('access_token', token, {
-      httpOnly: true,
-      secure: false,
-      sameSite: 'lax',
-    })
+    CookieService.assignAuthToken(res, token)
 
     return admin
   }
--- backend/src/common/cookies/cookie.service.ts
+++ backend/src/common/cookies/cookie.service.ts
@@ -1,5 +1,5 @@
 import { FastifyReply } from 'fastify';
-import { GUEST_TOKEN_COOKIE, SESSION_MAX_AGE } from '../../modules/menu/sessions/constants/session.constants';
+import { GUEST_TOKEN_COOKIE, SESSION_MAX_AGE, AUTH_COOKIE , AUTH_COOKIE_MAX_AGE } from '../../modules/menu/sessions/constants/session.constants';
 
 export class CookieService {
   static assignGuestToken(reply: FastifyReply, token: string) {
@@ -8,6 +8,14 @@ export class CookieService {
       sameSite: 'lax',
       path: '/',
       maxAge: SESSION_MAX_AGE,
+    });
+  }
+  static assignAuthToken(reply: FastifyReply, token: string) {
+    reply.setCookie(AUTH_COOKIE, token, {
+      httpOnly: true,
+      sameSite: 'lax',
+      path: '/',
+      maxAge: AUTH_COOKIE_MAX_AGE,
     });
   }
 }
