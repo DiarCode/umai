01a8ceed6bed96faf8222f573b91b6eeb88144b6
Adil-DD <adilashenov@gmail.com>
2026-04-01 12:15:00 +0500

    fix bd

---
+++ frontend/src/api/apiClient.ts
@@ -0,0 +1,9 @@
+// src/api/apiClient.ts
+import axios from 'axios';
+
+export const apiClient = axios.create({
+  baseURL: 'http://localhost:8080/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
